86bc80dd56160c986a605bc418ab70f401f231ca
Pody <113433675+Podygun@users.noreply.github.com>
2025-04-07 23:20:40 +0300

    test code of program

--- AdvancedDictionary/Program.cs
+++ AdvancedDictionary/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace AdvancedDictionary
+﻿namespace AdvancedDictionary;
+
+internal class Program
 {
-    internal class Program
+    static void Main( string[] args )
     {
-        static void Main(string[] args)
+        DictionaryManager m = new( "db.txt" );
+
+        while ( true )
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            string txt = Console.ReadLine();
+            var words = m.Get( txt );
+            foreach ( var word in words )
+            {
+                Console.WriteLine( word );
+            }
         }
     }
 }
